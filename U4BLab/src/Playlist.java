7dde3f565932091dd0c81e5a5a981bc42301645e
mostuniqueusername <prachi.snehaa@gmail.com>
2026-02-16 21:41:01 -0600

    Update Playlist.java
    added a way to read the file including try and catch methods as well as a toString method

--- U4BLab/src/Playlist.java
+++ U4BLab/src/Playlist.java
@@ -1,2 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Playlist {
+    private ArrayList<Song> songs = new ArrayList<Song>();
+
+    public Playlist() {
+        this.songs = songs;
+    }
+
+    public void readSongsFromFile(String name) {
+        try {
+            Scanner fileReader = new Scanner(new File("spotify_unique_years_artists.txt"));
+
+            while (fileReader.hasNextLine()) {
+
+                String line = fileReader.nextLine();
+                String[] parts = line.split(",");
+
+                String title = parts[0];
+                String artist = parts[1];
+                String album = parts[2];
+                int length = Integer.parseInt(parts[3]);
+                int year = Integer.parseInt(parts[4]);
+                String genre = parts[5];
+
+                Song s = new Song(title, artist, album, length, year, genre);
+                songs.add(s);
+            }
+
+            fileReader.close();
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+
+    public String toString() {
+
+        String result = "";
+
+        result += String.format("%-22s %-20s %-30s %-6s %-12s%n",
+                "Title", "Artist", "Album", "Year", "Genre");
+
+        result += "-------------------------------------------------------------------------------\n";
+
+        for (Song song : songs) {
+            result += song.toString() + "\n";
+        }
+
+        return result;
+    }
 }
