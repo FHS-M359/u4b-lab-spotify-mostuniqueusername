98181433a4e998a20f29896ab5f6dd124efd9ff0
mostuniqueusername <prachi.snehaa@gmail.com>
2026-02-16 23:11:52 -0600

    Update SpotifyTester.java
    updated so the file is read and formatted accordingly.

--- U4BLab/src/SpotifyTester.java
+++ U4BLab/src/SpotifyTester.java
@@ -1,2 +1,8 @@
 public class SpotifyTester {
+
+    public static void main(String[] args) {
+        Playlist playlist = new Playlist();
+        playlist.readSongsFromFile("U4BLab/spotify_unique_years_artists.txt");
+        System.out.println(playlist);
+    }
 }
