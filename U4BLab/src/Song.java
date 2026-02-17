727ad65d7619437235fedef1959bb3d6026b66c1
mostuniqueusername <prachi.snehaa@gmail.com>
2026-02-16 19:13:37 -0600

    Updated Song.java
    added variables & main constructor

--- U4BLab/src/Song.java
+++ U4BLab/src/Song.java
@@ -1,2 +1,19 @@
 public class Song {
+  private String title;
+  private String artist;
+  private String album;
+  private int length;
+  private int year;
+  private String genre;
+
+  public Song(String title, String artist, String album, int length, int year, String genre){
+    this.title = title;
+    this.artist = artist;
+    this.album = album;
+    this.length = length;
+    this.year = year;
+    this.genre = genre;
+  }
+
+  
 }
