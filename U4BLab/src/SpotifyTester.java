6a4d42429d09608cd9036495b8bb2e186a28b0c6
mostuniqueusername <prachi.snehaa@gmail.com>
2026-02-17 18:24:17 -0600

    Update SpotifyTester.java
    finished linear search for genres.

--- U4BLab/src/SpotifyTester.java
+++ U4BLab/src/SpotifyTester.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class SpotifyTester {
 
     public static void main(String[] args) {
         Playlist playlist = new Playlist();
         playlist.readSongsFromFile("U4BLab/spotify_unique_years_artists.txt");
         System.out.println(playlist);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What genre are you searching for?");
+        String genreSearching = input.nextLine();
+
+        playlist.searchByGenre(genreSearching);
     }
 }
